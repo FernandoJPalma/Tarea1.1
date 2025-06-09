091de567ba0c514d1ef778cd2a53ba5c8a1f6227
Fernando Palma <fjpalma@unah.hn>
2025-06-08 18:14:46 -0600

    Ejercicio 3

---
+++ ejercicio3.dart
@@ -0,0 +1,35 @@
+class Empleado {
+  String nombre;
+  String puesto;
+  double salario;
+
+  Empleado(this.nombre, this.puesto, this.salario);
+}
+
+double calcularSalarioTotal(List<Empleado> empleados) {
+  double total = 0;
+  for (var e in empleados) {
+    total += e.salario;
+  }
+  return total;
+}
+
+double calcularSalarioPromedio(List<Empleado> empleados) {
+  if (empleados.isEmpty) return 0;
+  return calcularSalarioTotal(empleados) / empleados.length;
+}
+
+void main() {
+  
+  List<Empleado> empleados = [
+    Empleado('Fernando', 'Desarrollador Junior', 1700),
+    Empleado('Mario', 'Desarrollador Junior', 16000),
+    Empleado('Juan', 'Catedratico', 23000),
+  ];
+
+  double total = calcularSalarioTotal(empleados);
+  double promedio = calcularSalarioPromedio(empleados);
+
+  print('\nSalario total: \Lps ${total}');
+  print('Salario promedio: \Lps ${promedio}');
+}
